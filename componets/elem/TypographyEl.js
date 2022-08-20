09ea9fbb90cf8123116bca8ef7c88d9a8bc8be86
Salvation27 <nikolayzalipykin@gmail.com>
2022-08-20 21:09:53 +0300

    first commit

---
+++ componets/elem/TypographyEl.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from "@mui/material";
+
+const TypographyEl = ({teg,classN}) => {
+  return (
+    <Typography component={teg} variant={classN}>
+      dfsdfsfdf
+    </Typography>
+  );
+}
+
+export default TypographyEl
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+const clientSideEmotionCache = createCache({ key: "css" });
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache
+}) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
 
 export default MyApp
